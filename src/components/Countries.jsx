cabf7a8e4b61c81b8459c2fb047e59e13bb09cec
Talal Mahmood <talalmahmud2011@gmail.com>
2023-02-14 08:24:19 +0600

    change

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,59 +1,55 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import Country from './Country'
-import './Countries.css'
-import Form from './Form'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Country from "./Country";
+import "./Countries.css";
+import Form from "./Form";
 
-const URL = 'https://restcountries.com/v3.1/all'
-
+const URL = "https://restcountries.com/v3.1/all";
 
 const Countries = () => {
-    const [countries, setCountries] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    const [isError, setIsError] = useState('')
-    const [totalCountries, setTotalCountries] =useState(countries)
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState("");
+  const [totalCountries, setTotalCountries] = useState(countries);
 
-    const fetchData = async (url) =>{
-        try {
-            let response = await fetch(url)
-            let data = await response.json()
-            setCountries(data)
-            setTotalCountries(data)
+  const fetchData = async (url) => {
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      setCountries(data);
+      setTotalCountries(data);
 
-            setIsLoading(false)
-           // console.log(countries)
-        } catch (error) {
-            setIsError(error.message)
-        }
+      setIsLoading(false);
+      // console.log(countries)
+    } catch (error) {
+      setIsError(error.message);
     }
+  };
 
-    useEffect(()=>{
-        fetchData(URL)
-    },[]);
-    
+  useEffect(() => {
+    fetchData(URL);
+  }, []);
 
-    const searchCountry = (name)=>{
-        let newCountries = countries.filter((country) =>{
-            const countryName = country.name.common.toLowerCase()
-            return countryName.startsWith(name)
-        })
-        setTotalCountries(newCountries)  
-    }
+  const searchCountry = (name) => {
+    let newCountries = countries.filter((country) => {
+      const countryName = country.name.common.toLowerCase();
+      return countryName.startsWith(name);
+    });
+    setTotalCountries(newCountries);
+  };
 
-    
   return (
-    <div className='countries'>
-        {isLoading && <p>Loading.......</p>}
-        {isError && <p>{isError}</p>}
-        <Form getSearchdata={searchCountry} />
+    <div className="countries">
+      {isLoading && <p>Loading.......</p>}
+      {isError && <p>{isError}</p>}
+      <Form getSearchdata={searchCountry} />
 
-
-        {countries && totalCountries.map((country) => {
-            return <Country key={country.name.common} country={country} />
+      {countries &&
+        totalCountries.map((country) => {
+          return <Country key={country.name.common} country={country} />;
         })}
-
     </div>
-  )
-}
+  );
+};
 
-export default Countries
+export default Countries;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,30 @@
-import React, { memo, useState } from 'react'
-import { useEffect } from 'react'
-import './Form.css'
+import React, { memo, useState } from "react";
+import { useEffect } from "react";
+import "./Form.css";
 
 const Form = (props) => {
-    const [seracCountry, setSearchCountry] = useState("")
-    //const [name, setName] = useState('')
-    const {getSearchdata} = props
+  const [serachCountry, setSearchCountry] = useState("");
+  //const [name, setName] = useState('')
+  const { getSearchdata } = props;
 
-    const handleSearch = (e) =>{
-        setSearchCountry(e.target.value)
-    }
+  const handleSearch = (e) => {
+    setSearchCountry(e.target.value);
+  };
 
-    useEffect(()=>{
-         getSearchdata(seracCountry.toLowerCase())
-    },[seracCountry])
-    
+  useEffect(() => {
+    getSearchdata(serachCountry.toLowerCase());
+  }, [serachCountry, getSearchdata]);
 
   return (
     <div>
-        <input type="text" value={seracCountry} placeholder='Enter country' onChange={handleSearch}/>
+      <input
+        type="text"
+        value={serachCountry}
+        placeholder="Enter country"
+        onChange={handleSearch}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default memo (Form)
+export default memo(Form);
